d0248fdf6c93ac19208d53eb8ab785e045be4162
MohammedHussain <68030846+Moh9998@users.noreply.github.com>
2024-12-01 16:02:29 +0300

    Adding MIgrations And configuring Few Minor modifications on the validation scope and upadting

--- MyContacts/Models/Contact.cs
+++ MyContacts/Models/Contact.cs
@@ -17,7 +17,8 @@ namespace MyContacts.Models
         [Required(ErrorMessage = "Please enter a phone number")]
         public string PhoneNumber { get; set; } = null!;
 
-        [Required(ErrorMessage = "Please enter an email")]
+        [Required(ErrorMessage = "Please enter an email"),
+         EmailAddress(ErrorMessage ="Please Enter Valid Email")]
         public string Email { get; set; } = null!;
 
         //FK for the Category table
